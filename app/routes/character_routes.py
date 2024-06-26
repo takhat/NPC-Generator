c76c5498b2770cbc9fa0b64d8c1c5d423d389686
Roshni <roshnipohuja@gmail.com>
2024-06-26 12:07:58 -0700

    incorporate llama-routes working

--- app/routes/character_routes.py
+++ app/routes/character_routes.py
@@ -1,9 +1,15 @@
+import os
 from flask import Blueprint, jsonify, request, abort, make_response
 from ..db import db
 from ..models.character import Character
 from ..models.greeting import Greeting
 from sqlalchemy import func, union, except_
+from openai import OpenAI
 
+client = OpenAI(
+api_key = os.environ.get("LLAMA_API_KEY"),
+base_url = "https://api.llama-api.com"
+)
 
 bp = Blueprint("characters", __name__, url_prefix="/characters")
 
@@ -43,11 +49,39 @@ def get_characters():
 
 @bp.get("/<char_id>/greetings")
 def get_greetings(char_id):
-    pass
+    character=validate_model(Character,char_id)
+    if not character.greetings:
+        return make_response(
+            jsonify(
+                f"No greetings found for {character.name}"), 201)
 
 @bp.post("/<char_id>/generate")
 def add_greetings(char_id):
-    pass
+    character=validate_model(Character,char_id)
+    greetings=generate_greeting(character)
+    if character.greetings:
+        return make_response(
+            jsonify(f"Greetings already exist for {character.name}"), 201)     
+    new_greetings = []
+    for greeting in greetings:
+        new_greeting = Greeting(
+            greeting_text = greeting[greeting.find(" ")+1:].strip("\""),
+            character = character
+        )
+        new_greetings.append(new_greeting)
+        db.session.add_all(new_greetings)
+        db.session.commit()
+        return make_response(jsonify(f"Greetings successfully added to {character.name}"), 201)
+def generate_greeting(character):
+    prompt = f"I am writing a fantasy style role playing video game. I have an npc named {character.name} who is {character.age}.They are a {character.occupation} who has a {character.personality} personality. Can you help me python style list of 10 stock phrases they might use when another character talks to them?"
+    response = client.chat.completions.create(
+        # model="gpt-3.5-turbo",
+        model="llama-13b-chat",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+    return response.choices[0].message.content.split("\n")
 
 def validate_model(cls,id):
     try:
